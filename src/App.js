0bc3a9aa175325aa2cb1fd8826d649db542dd4fb
Arya-Chandran <aryachandran86@gmail.com>
2022-01-12 12:05:53 -0500

    passing data from child to parent component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-const App=() => {
+const App = () => {
   const expenses = [
-    { id:'e1', title: "Toilet paper", amount: 94.12, date: new Date(2020, 7, 14) },
-    { id:'e2', title: "New TV", amount:799.49, date: new Date(2021, 2, 12) },
-    { id:'e3', title: "Car insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-    { id:'e4', title: "New Desk(wooden)", amount: 450, date: new Date(2021, 5, 12) },
+    {
+      id: "e1",
+      title: "Toilet paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk(wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
+
+  const addExpenseHandler= expense =>{
+    console.log('In App.js');
+    console.log(expense );
+  }
   return (
     <div className="App">
-      <h2>Let's get started</h2>
-       <Expenses items={expenses} />
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.scss';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses =(props) => {
+    const[filteredYear,setFilteredYear]=useState('2020');
+
+    const filterChangeHandler =selectedYear =>{
+        console.log('Expenses.js');
+        setFilteredYear(selectedYear);
+
+    }
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
               <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
               <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
               <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
